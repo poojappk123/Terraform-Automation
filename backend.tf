5b89fe98caf0ddef7956719157704e0a5a08f5b8
poojappk123 <127734175+poojappk123@users.noreply.github.com>
2025-03-18 16:34:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-batch-26"
+    bucket = "pooja-shirke-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
